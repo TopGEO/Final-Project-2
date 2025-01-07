54743a46160c3e291270f745d5ca4a0e6feecd80
TopGEO <nikanatroshvili10@gmail.com>
2025-01-07 19:02:48 +0400

    BrowserStack removed from main (added in BrowserStack branch)

--- src/test/java/ge/tbc/testautomation/BaseTest.java
+++ src/test/java/ge/tbc/testautomation/BaseTest.java
@@ -2,86 +2,35 @@ package ge.tbc.testautomation;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.File;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-
 public class BaseTest {
     public SoftAssert sfa = new SoftAssert();
-    public RemoteWebDriver driver;
-    public static String userName, accessKey;
-    public static Map<String, Object> browserStackYamlMap;
-    public static final String USER_DIR = "user.dir";
-
-    public BaseTest() {
-        File file = new File(getUserDir() + "/browserstack.yml");
-        this.browserStackYamlMap = convertYamlFileToMap(file, new HashMap<>());
-        userName = System.getenv("BROWSERSTACK_USERNAME") != null ? System.getenv("BROWSERSTACK_USERNAME") : (String) browserStackYamlMap.get("userName");
-        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null ? System.getenv("BROWSERSTACK_ACCESS_KEY") : (String) browserStackYamlMap.get("accessKey");
-    }
 
     // we will use BeforeMethod and AfterMethod (its required in homework)
+    // this tells TestNG that "even if im filtering test methods by group, run this @BeforeMethod anyway"
     @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        MutableCapabilities capabilities = new MutableCapabilities();
-        HashMap<String, String> bstackOptions = new HashMap<>();
-        bstackOptions.put("source", "selenide:sample-master:v1.2");
-        bstackOptions.put("resolution", "1920x1080");
-        capabilities.setCapability("bstack:options", bstackOptions);
-        driver = new RemoteWebDriver(new URL(String.format("https://%s:%s@hub-cloud.browserstack.com/wd/hub", userName, accessKey)), capabilities);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebDriverRunner.setWebDriver(driver);
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1366,728");
+        WebDriverRunner.setWebDriver(new ChromeDriver(options));
+        Configuration.browserCapabilities = options;
+        Configuration.browser = "chrome";
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        Configuration.timeout = 10000;
     }
-
-    // local testing if needed
-//    @BeforeMethod(alwaysRun = true) // this tells TestNG that "even if im filtering test methods by group, run this @BeforeMethod anyway"
-//    public void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless", "--disable-gpu", "--window-size=1366,728");
-//        WebDriverRunner.setWebDriver(new ChromeDriver(options));
-//        Configuration.browserCapabilities = options;
-//        Configuration.browser = "chrome";
-//        WebDriverRunner.getWebDriver().manage().window().maximize();
-//        Configuration.timeout = 10000;
-//    }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        WebDriverRunner.getWebDriver().quit();
+        getWebDriver().quit();
     }
 
-    private String getUserDir() {
-        return System.getProperty(USER_DIR);
-    }
-
-    private Map<String, Object> convertYamlFileToMap(File yamlFile, Map<String, Object> map) {
-        try {
-            InputStream inputStream = Files.newInputStream(yamlFile.toPath());
-            Yaml yaml = new Yaml();
-            Map<String, Object> config = yaml.load(inputStream);
-            map.putAll(config);
-        } catch (Exception e) {
-            throw new RuntimeException(String.format("Malformed browserstack.yml file - %s.", e));
-        }
-        return map;
-    }
-
+    @AfterSuite()
     @AfterClass(alwaysRun = true) // after class is needed, since testng.xml's filtering ignores if its AfterSuite
     public void tearDownClass() {
         sfa.assertAll();
