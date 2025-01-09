33682818738d7ed5c0cece94488f00a9e9ec7dc8
TopGEO <nikanatroshvili10@gmail.com>
2025-01-09 15:51:31 +0400

    added Explanation

--- src/main/java/ge/tbc/testautomation/steps/CommonSteps.java
+++ src/main/java/ge/tbc/testautomation/steps/CommonSteps.java
@@ -6,12 +6,14 @@ public class CommonSteps<T extends CommonSteps<T>> {
     // single shared SoftAssert for all tests
     protected static SoftAssert sfa = new SoftAssert();
 
+
+    // we have to do assert all after all test to take screenshots (screenshot can be taken only when fail is detected during Test and another Test method is not started
     @SuppressWarnings("unchecked")
     public T checkSoftAssert() {
         try {
             CommonSteps.sfa.assertAll();
         } finally {
-            // Re-init so next test doesn’t see old errors
+            // re-init so next test doesn’t see old errors
             sfa = new SoftAssert();
         }
 
